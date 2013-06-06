ef44e480a2b5f0dcdf4a10dbcfa675c956ecd2b6
alphalemon <user@server.com>
2013-06-06 12:56:10 +0200

    added copyright

--- Resources/public/js/thumbnail_editor.js
+++ Resources/public/js/thumbnail_editor.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
+/*
+ * This file is part of the BootstrapThumbnailBlockBundle and it is distributed
+ * under the MIT LICENSE. To use this application you must leave intact this copyright 
+ * notice.
+ *
+ * Copyright (c) AlphaLemon <webmaster@alphalemon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://www.alphalemon.com
+ * 
+ * @license    MIT LICENSE
+ * 
+ */
+ 
+ $(document).ready(function() {
     $(document).on("blockEditing", function(event, element){
         if (element.attr('data-type') != 'BootstrapThumbnailsBlock') {
             return;
